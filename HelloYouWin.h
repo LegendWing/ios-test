c46dac53b025e05bd458ac72f0f90a3440ab9702
Cadrick <cadrickloh@gmail.com>
2012-09-24 23:59:17 +0800

    Added 2 new files, HelloYouWin But I'm not gonna fucking pushing this xproj!

---
+++ HelloYouWin.h
@@ -0,0 +1,16 @@
+//
+//  HelloYouWin.h
+//  gittest
+//
+//  Created by Cadrick on 9/24/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface HelloYouWin : NSObject {
+    
+}
+
+@end
---
+++ HelloYouWin.m
@@ -0,0 +1,14 @@
+//
+//  HelloYouWin.m
+//  gittest
+//
+//  Created by Cadrick on 9/24/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelloYouWin.h"
+
+
+@implementation HelloYouWin
+
+@end
