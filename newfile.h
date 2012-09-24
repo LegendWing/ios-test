9faa940638c961e00cd465b2762cb65b345a0148
Cadrick <cadrickloh@gmail.com>
2012-09-25 02:41:40 +0800

    Added 2 new files

---
+++ newfile.h
@@ -0,0 +1,16 @@
+//
+//  newfile.h
+//  gittest
+//
+//  Created by Cadrick on 9/25/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface newfile : NSObject {
+    
+}
+
+@end
---
+++ newfile.m
@@ -0,0 +1,14 @@
+//
+//  newfile.m
+//  gittest
+//
+//  Created by Cadrick on 9/25/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "newfile.h"
+
+
+@implementation newfile
+
+@end
