6e8aa6dc012c822004b158f497412b329d2ee7bd
Cadrick <cadrickloh@gmail.com>
2012-09-24 23:50:35 +0800

    added 2 new files... i didnt commit my other changes because they are stil in progress.

---
+++ Haha.c
@@ -0,0 +1,9 @@
+//
+//  Haha.c
+//  gittest
+//
+//  Created by Cadrick on 9/24/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include "Haha.h"
---
+++ Haha.h
@@ -0,0 +1,7 @@
+//
+//  Haha.h
+//  gittest
+//
+//  Created by Cadrick on 9/24/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
