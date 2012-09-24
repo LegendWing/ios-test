2ef2aa80aee34ddfe503d8f475d3708ead44a2e4
LegendWing <youwin1990@gmail.com>
2012-09-24 23:19:18 +0800

    another test

--- gittest/DetailViewController.h
+++ gittest/DetailViewController.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+// teste stests
+
 #import <UIKit/UIKit.h>
 
 @interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
