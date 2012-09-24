b67648878a87bbe447313aee5837c6f797f3367d
Cadrick <cadrickloh@gmail.com>
2012-09-24 23:46:41 +0800

    aaa

---
+++ Zzz.c
@@ -0,0 +1,9 @@
+//
+//  Zzz.c
+//  gittest
+//
+//  Created by Cadrick on 9/24/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include "Zzz.h"
---
+++ Zzz.h
@@ -0,0 +1,7 @@
+//
+//  Zzz.h
+//  gittest
+//
+//  Created by Cadrick on 9/24/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
