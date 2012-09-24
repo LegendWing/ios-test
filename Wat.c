aae297dad929c6173ab5adc7f89ce4846105b104
Cadrick <cadrickloh@gmail.com>
2012-09-24 23:39:32 +0800

    Added wat.c and wat.h

---
+++ Wat.c
@@ -0,0 +1,9 @@
+//
+//  Wat.c
+//  gittest
+//
+//  Created by Cadrick on 9/24/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include "Wat.h"
---
+++ Wat.h
@@ -0,0 +1,7 @@
+//
+//  Wat.h
+//  gittest
+//
+//  Created by Cadrick on 9/24/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
