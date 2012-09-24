c17e16be75c50f2bcc24fccba1e933b8e4da70d1
LegendWing <youwin1990@gmail.com>
2012-09-25 00:06:22 +0800

    adding test

---
+++ siou.h
@@ -0,0 +1,13 @@
+//
+//  siou.h
+//  gittest
+//
+//  Created by Yeap You Win on 9/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface siou : NSObject
+
+@end
---
+++ siou.m
@@ -0,0 +1,13 @@
+//
+//  siou.m
+//  gittest
+//
+//  Created by Yeap You Win on 9/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "siou.h"
+
+@implementation siou
+
+@end
